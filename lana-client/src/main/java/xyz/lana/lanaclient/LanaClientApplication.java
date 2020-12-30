cd0c8b9a8c78bdd117bab463d0771ab3618e2c80
tfdsimoes <tfdsimoes@gmail.com>
2020-12-30 18:04:55 +0100

    Set lana-client project

---
+++ lana-client/src/main/java/xyz/lana/lanaclient/LanaClientApplication.java
@@ -0,0 +1,13 @@
+package xyz.lana.lanaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanaClientApplication.class, args);
+	}
+
+}
