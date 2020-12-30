b8486fc0bdbee0b6a58b37772293d7debe0e3358
tfdsimoes <tfdsimoes@gmail.com>
2020-12-30 21:24:55 +0100

    Adaptation to docker-compose deployment

--- lana-server/src/main/java/xyz/lana/lanaserver/repository/InformationRepository.java
+++ lana-server/src/main/java/xyz/lana/lanaserver/repository/InformationRepository.java
@@ -3,16 +3,18 @@ package xyz.lana.lanaserver.repository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import xyz.lana.lanaserver.entity.Product;
 import xyz.lana.lanaserver.entity.Promotion;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Class that handles the load of file to memory with Products and Promotions
@@ -34,15 +36,21 @@ public class InformationRepository {
 
     @PostConstruct
     public void init() throws IOException {
-        // Load products to memory
-        File productsFile = new ClassPathResource(productsPath).getFile();
-        products = objectMapper.readValue(productsFile, new TypeReference<>() {
-        });
 
-        // Load promotions to memory
-        File promotionsFile = new ClassPathResource(promotionsPath).getFile();
-        promotions = objectMapper.readValue(promotionsFile, new TypeReference<>() {
-        });
+        // Load products to memory
+        try (InputStream inputStream = InformationRepository.class.getResourceAsStream(productsPath)) {
+
+            String contentFile = new BufferedReader(new InputStreamReader(inputStream)).lines().parallel().collect(Collectors.joining("\n"));
+            products = objectMapper.readValue(contentFile, new TypeReference<>() {
+            });
+        }
+
+        try (InputStream inputStream = InformationRepository.class.getResourceAsStream(promotionsPath)) {
+            // Load promotions to memory
+            String contentFile = new BufferedReader(new InputStreamReader(inputStream)).lines().parallel().collect(Collectors.joining("\n"));
+            promotions = objectMapper.readValue(contentFile, new TypeReference<>() {
+            });
+        }
     }
 
     public List<Product> getProducts() {
